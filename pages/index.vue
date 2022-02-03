0c23427c7ab7b26881f9b9fb5eeec3b7f11f884a
Sidhanth surana <sidhanth.surana12@gmail.com>
2022-02-03 19:13:58 +0530

    stable

---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <h2>Test the font yo</h2>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+}
+</script>
---
+++ pages/login.vue
@@ -0,0 +1,15 @@
+
+<template>
+    <div class="login-page__main-container">
+        Hi this is login
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'LoginPage'
+}
+</script>
+<style>
+ @import '~/assets/styles/login.scss'
+</style>
