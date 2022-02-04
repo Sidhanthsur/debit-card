5e14d2c705cbc0a6724b99168484da51f6f77ab0
Sidhanth surana <sidhanth.surana12@gmail.com>
2022-02-04 20:59:06 +0530

    stable

--- pages/index.vue
+++ pages/index.vue
@@ -12,21 +12,40 @@
           </div>
           <currency-unit />
         </div>
-        <add-new-card @onClick="showModal=true" />
+        <add-new-card @onClick="showModal = true" />
       </div>
     </div>
-   <div v-if="debitCards && debitCards.length" class="card-action__container">
-    <agile v-if="showCarousel" ref="carousel" :dots="true" :infinite="false" :nav-buttons="false" class="carousel-container mr-4" @after-change="setCurrentSlide($event)">
-      <debit-card v-for="card in debitCards" :key="card.id" :card="card" />
-    </agile>
-    <bottom-options-bar :current-card-frozen="currentCardFrozen" :class="{'mx-auto': isMobile}" @cancel-card="showCancelModal = true" @freeze-card="toggleCardFreeze(currentCard.id)"/>
-   </div>
-        <Dialog v-if="showModal">
-          <add-new-card-modal @on-submit="onAddNewCard" @cancel="showModal=false" />
-        </Dialog>
-          <Dialog v-if="showCancelModal">
-            <cancel-card @remove-card="onRemoveCard" @cancel="showCancelModal=false"/>
-        </Dialog>
+    <div v-if="debitCards && debitCards.length" class="card-action__container">
+      <agile
+        v-if="showCarousel"
+        ref="carousel"
+        :dots="true"
+        :infinite="false"
+        :nav-buttons="false"
+        class="carousel-container mr-4"
+        @after-change="setCurrentSlide($event)"
+      >
+        <debit-card v-for="card in debitCards" :key="card.id" :card="card" />
+      </agile>
+      <bottom-options-bar
+        :current-card-frozen="currentCardFrozen"
+        :class="{ 'mx-auto': isMobile }"
+        @cancel-card="showCancelModal = true"
+        @freeze-card="onToggleFreeze"
+      />
+    </div>
+    <Dialog v-if="showModal">
+      <add-new-card-modal
+        @on-submit="onAddNewCard"
+        @cancel="showModal = false"
+      />
+    </Dialog>
+    <Dialog v-if="showCancelModal">
+      <cancel-card
+        @remove-card="onRemoveCard"
+        @cancel="showCancelModal = false"
+      />
+    </Dialog>
   </div>
 </template>
 
@@ -40,45 +59,55 @@ import { mapState, mapActions } from "vuex";
 import AddNewCard from "~/components/AddNewCard.vue";
 
 import DebitCard from "~/components/DebitCard.vue";
-import Dialog from '~/components/Dialog.vue';
-import AddNewCardModal from '~/components/AddNewCardModal.vue';
-import CancelCard from '~/components/CancelCard.vue';
+import Dialog from "~/components/Dialog.vue";
+import AddNewCardModal from "~/components/AddNewCardModal.vue";
+import CancelCard from "~/components/CancelCard.vue";
 export default {
   name: "IndexPage",
-  components: { DebitCard, AddNewCard, agile: VueAgile, Dialog, AddNewCardModal, CancelCard },
-  data () {
+  components: {
+    DebitCard,
+    AddNewCard,
+    agile: VueAgile,
+    Dialog,
+    AddNewCardModal,
+    CancelCard
+  },
+  data() {
     return {
       showModal: false,
       currentCard: null,
       showCarousel: true,
       showCancelModal: false
-    }
+    };
   },
   computed: {
-    ...mapState('cards',['debitCards']),
-    currentCardFrozen () {
+    ...mapState("cards", ["debitCards"]),
+    currentCardFrozen() {
       return this.currentCard?.frozen || false;
     }
   },
   methods: {
-  ...mapActions('cards',['toggleCardFreeze','removeCard', 'addCard']),
-  setCurrentSlide(event) {
-    this.currentCard = this.debitCards[event.currentSlide]
-  },
-    async onRemoveCard () {
-      this.showCarousel = false
-      await this.removeCard(this.currentCard.id)
+    ...mapActions("cards", ["toggleCardFreeze", "removeCard", "addCard"]),
+    setCurrentSlide(event) {
+      this.currentCard = this.debitCards[event.currentSlide];
+    },
+    async onRemoveCard() {
+      this.showCarousel = false;
+      await this.removeCard(this.currentCard.id);
       this.showCarousel = true;
       this.showCancelModal = false;
     },
-      async onAddNewCard (name) {
-     this.showCarousel = false
-    await this.addCard(name)
+    async onAddNewCard(name) {
+      this.showCarousel = false;
+      await this.addCard(name);
       this.showCarousel = true;
       this.showModal = false;
+    },
+    onToggleFreeze() {
+      this.toggleCardFreeze(this.currentCard.id);
+      this.currentCard.frozen = !this.currentCard.frozen;
+    }
   }
-  }
-
 };
 </script>
 <style lang="scss" scoped>
@@ -106,6 +135,5 @@ export default {
   .card-action__container {
     margin-left: 0px;
   }
-
 }
 </style>
