e6ef5e7428c8ad5c984dc2c0f420780a19079f98
Sidhanth surana <sidhanth.surana12@gmail.com>
2022-02-05 17:41:41 +0530

    code improvements

--- components/AddNewCard.vue
+++ components/AddNewCard.vue
@@ -1,47 +1,46 @@
 <template>
-    <div class="flex items-center box-container box-container--responsive" @click="$emit('onClick')">
-       <img class="logo" :src="isMobile ? addMobile : add" alt="add-card" />
-       <span class="ml-2">New Card</span>
-    </div>
+  <div
+    class="flex items-center new-card__container new-card__container--responsive"
+    @click="$emit('onClick')"
+  >
+    <img class="logo" :src="isMobile ? addMobile : add" alt="add-card" />
+    <span class="ml-2">New Card</span>
+  </div>
 </template>
 <script>
-import add from '~/assets/images/add.svg'
-import addMobile from '~/assets/images/addMobile.svg'
+import add from "~/assets/images/add.svg";
+import addMobile from "~/assets/images/addMobile.svg";
 
 export default {
-    created () {
-        this.add = add;
-        this.addMobile = addMobile
-    }
-}
+  created() {
+    this.add = add;
+    this.addMobile = addMobile;
+  }
+};
 </script>
 <style lang="scss">
+.new-card__container {
+  height: 35px;
+  padding: 10px;
+  font-weight: bold;
+  font-size: 12px;
+}
+.new-card__container--responsive {
+  height: 35px;
+  background-color: #325baf;
+  border-radius: 4px;
+  padding: 10px;
+  color: white;
+}
 
-    .box-container {
-       height: 35px;  
-       padding: 10px;
-       font-weight: bold;
-       font-size: 12px;
-    }
-    .box-container--responsive {
-        height: 35px;
-        background-color: #325BAF;
-        border-radius: 4px;
-        padding: 10px;
-        color: white;
+.logo {
+  @include square(16px);
+}
 
-    }
-
-    .logo {
-        @include square(16px)
-    }
-
-    @media only screen and (max-width: 600px) {
-    .box-container--responsive {
-        background-color: transparent;
-        color: #23CEFD;
-    }
-    }
-
-    
+@media only screen and (max-width: 600px) {
+  .new-card__container--responsive {
+    background-color: transparent;
+    color: #23cefd;
+  }
+}
 </style>
--- components/AddNewCardModal.vue
+++ components/AddNewCardModal.vue
@@ -1,55 +1,70 @@
 <template>
-    <div class="new-card__main-container">
-        <h1 class="mb-6">Adding a new card</h1>
-        <input v-model.trim="name" class="input-container p-2" placeholder="Enter card holder name" type="text" @input="error = false">
-        <div v-if="error" class="text-red-600">Please enter a first name and a last name</div>
-        <div class="mt-4">
-            <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" @click="onSubmit">Submit</button>
-            <button
-             class="bg-transparent text-blue-700 font-semibold py-2 px-4 border border-blue-500 rounded"
-              @click="$emit('cancel')">Cancel</button>
-        </div>
+  <div class="new-card-modal__main-container">
+    <h1 class="mb-6">Adding a new card</h1>
+    <input
+      v-model.trim="name"
+      class="new-card-modal__input-container p-2"
+      placeholder="Enter card holder name"
+      type="text"
+      @input="error = false"
+    />
+    <div v-if="error" class="text-red-600">
+      Please enter a first name and a last name
     </div>
+    <div class="mt-4">
+      <button
+        class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        @click="onSubmit"
+      >
+        Submit
+      </button>
+      <button
+        class="bg-transparent text-blue-700 font-semibold py-2 px-4 border border-blue-500 rounded"
+        @click="$emit('cancel')"
+      >
+        Cancel
+      </button>
+    </div>
+  </div>
 </template>
 <script>
 export default {
-  data () {
-      return {
-          name: '',
-          error: false
-      }
+  data() {
+    return {
+      name: "",
+      error: false
+    };
   },
   methods: {
-      onSubmit() {
-          if (/\s/g.test(this.name)) {
-            this.$emit('on-submit', this.name)
-          } else {
-              this.error = true;
-          }
-        
+    onSubmit() {
+      if (/\s/g.test(this.name)) {
+        this.$emit("on-submit", this.name);
+      } else {
+        this.error = true;
       }
-  } 
-}
+    }
+  }
+};
 </script>
 <style lang="scss" scoped>
-.new-card__main-container {
-    background-color: white;
-    width: 40%;
-    height: 200px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    flex-direction: column;
-    border-radius: 14px;
+.new-card-modal__main-container {
+  background-color: white;
+  width: 40%;
+  height: 200px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  border-radius: 14px;
 }
 
-.input-container {
-    border: 1px solid black;
-    border-radius: 4px;
-    width: 90%;
+.new-card-modal__input-container {
+  border: 1px solid black;
+  border-radius: 4px;
+  width: 90%;
 }
-   @media only screen and (max-width: 600px) {
-   .new-card__main-container {
+@media only screen and (max-width: 600px) {
+  .new-card-modal__main-container {
     background-color: white;
     width: 90%;
     height: 200px;
@@ -58,7 +73,6 @@ export default {
     align-items: center;
     flex-direction: column;
     border-radius: 10px;
+  }
 }
-   }
-
 </style>
