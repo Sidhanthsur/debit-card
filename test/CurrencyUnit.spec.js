4f501d283e63fe652dcab7e12e8d7a5a4fcbf903
Sidhanth surana <sidhanth.surana12@gmail.com>
2022-02-06 16:47:59 +0530

    unit case

--- test/CurrencyUnit.spec.js
+++ test/CurrencyUnit.spec.js
@@ -4,11 +4,19 @@ import CurrencyUnit from '@/components/CurrencyUnit.vue'
 describe('Component: CurrencyUnit', () => {
   let wrapper;
   beforeEach(() => {
-    wrapper = mount(CurrencyUnit)
+    wrapper = mount(CurrencyUnit, {
+      propsData: {
+        currencyValue: 5000
+      }
+    })
   })
   
   test('Main Container is visible', () => {
     expect(wrapper.find('[data-test="main-container"]').exists()).toBe(true)
+  })
+
+  test('Computed property: currencyValueFormatted returns the correct value', () => {
+    expect(wrapper.vm.currencyValueFormatted).toBe('5,000')
   })
   
   afterEach(() => {
