4980899beda7dfe7404a240513075e1bbc3aa6ae
Sidhanth surana <sidhanth.surana12@gmail.com>
2022-02-06 17:52:11 +0530

    Updates

--- _nuxt/static/1643907423/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fdebit-card\u002F_nuxt\u002Fstatic\u002F1643907423",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Fdebit-card\u002F",assetsPath:"\u002Fdebit-card\u002F_nuxt\u002F",cdnURL:a}}}}(null));
