65292069836daa45912f12df30ce6992eb4f0f34
Sidhanth surana <sidhanth.surana12@gmail.com>
2022-02-06 15:55:52 +0530

    code improvements

--- components/DebitCard.vue
+++ components/DebitCard.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="relative z-10 main-container">
-    <div class="tappable-container" @click="toggleEyeClick">
+  <div class="relative z-10 debit__main-container">
+    <div class="debit__tappable-container" @click="toggleEyeClick">
       <img
         :src="showCardNumber ? hideEye : showEye"
         alt="eye-icon"
-        class="eye-icon"
+        class="debit__eye-icon"
       />
       <div class="ml-1">
         {{ showCardNumber ? "Hide card number" : "Show card number" }}
@@ -12,29 +12,29 @@
     </div>
 
     <div
-      class="card-container"
-      :class="{ 'card-container--frozen': card.frozen }"
+      class="debit__card-container"
+      :class="{ 'debit__card-container--frozen': card.frozen }"
     >
-      <img class="card-logo" src="~/static/aspireLong.svg" alt="aspire-logo" />
-      <h1 class="default-margin">
+      <img class="debit__card-logo" src="~/static/aspireLong.svg" alt="aspire-logo" />
+      <h1 class="debit__default-margin">
         {{ card.name }}
       </h1>
-      <div class="default-margin number-container">
+      <div class="debit__default-margin debit__number-container">
         <div
           v-for="(group, index) in arrayOfCardNumbers"
           :key="index"
-          class="number"
+          class="debit__number"
         >
           {{ group }}
         </div>
       </div>
 
-      <div class="bottom-container">
+      <div class="debit__bottom-container">
         <div>Thru: {{ card.exp.month }} / {{ card.exp.year }}</div>
-        <div class="cvv">CVV: {{ showCardNumber ? card.cvv : "* * *" }}</div>
+        <div class="debit__cvv">CVV: {{ showCardNumber ? card.cvv : "* * *" }}</div>
       </div>
 
-      <img class="card-logo" src="~/static/visaLogo.svg" alt="visa-logo" />
+      <img class="debit__card-logo" src="~/static/visaLogo.svg" alt="visa-logo" />
     </div>
   </div>
 </template>
@@ -78,22 +78,18 @@ export default {
 @import "~assets/scss/colors";
 @import "~assets/scss/variables";
 
-.main-container {
+.debit__main-container {
   width: 366px;
   height: 220px;
 }
 
-.tappable-container {
+.debit__tappable-container {
   background-color: white;
   width: 150px;
   height: $default-size-unit;
   margin-left: auto;
   border-top-left-radius: 6px;
   border-top-right-radius: 6px;
-  right: 0;
-  top: -$default-spacing-unit;
-  bottom: 10px;
-  z-index: -1;
   font-size: 12px;
   color: $aspire-green;
   display: flex;
@@ -102,51 +98,51 @@ export default {
   cursor: pointer;
 }
 
-.eye-icon {
+.debit__eye-icon {
   @include square(16px);
 }
 
-.card-container {
+.debit__card-container {
   background-color: $aspire-green;
   border-radius: 12px;
   color: white;
   padding: $default-spacing-unit;
 }
 
-.card-container--frozen {
+.debit__card-container--frozen {
   background-color: grey;
 }
 
-.default-margin {
+.debit__default-margin {
   margin-top: $default-spacing-unit;
 }
 
-.card-logo {
+.debit__card-logo {
   margin-left: auto;
 }
 
-.number-container {
+.debit__number-container {
   @include mediumText;
   display: flex;
   font-weight: 600;
 }
 
-.number {
+.debit__number {
   margin-right: $default-spacing-unit;
 }
 
-.bottom-container {
+.debit__bottom-container {
   @include mediumText;
   font-weight: 600;
   display: flex;
   margin-top: 14px;
 }
 
-.cvv {
+.debit__cvv {
   margin-left: $default-spacing-unit;
 }
 @media only screen and (max-width: 768px) {
-  .main-container {
+  .debit__main-container {
     width: 320px;
     height: 220px;
   }
